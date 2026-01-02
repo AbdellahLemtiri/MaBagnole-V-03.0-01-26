c4e0d6948716c0ffa567be75655bfa1ea4d21e71
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-02 11:50:35 +0100

    feat: Implement dashboard route in index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-// 1. ديما سبق Autoloader هو اللول
+
 require_once __DIR__ . '/App/Autoloader.php';
 App\Autoloader::register();
 
-// 2. ضروري دير import للكلاس بالمسار الكامل ديالها
+
 use App\Controllers\AuthController; 
 
 
