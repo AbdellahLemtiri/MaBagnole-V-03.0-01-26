814f8d0181de5026c4bcfde593a88659f8472d87
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-02 10:47:18 +0100

    refactor: Optimize Autoloader with PSR-4 standards

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// 1. ديما سبق Autoloader هو اللول
+require_once __DIR__ . '/App/Autoloader.php';
+App\Autoloader::register();
+
+// 2. ضروري دير import للكلاس بالمسار الكامل ديالها
+use App\Controllers\AuthController; 
+
+// 3. دابا عاد تقدر تعيط ليها
+$authController = new AuthController();
+
+$action = $_POST['action'] ?? $_GET['action'] ?? 'auth';
+
+switch ($action) {
+    case 'login':
+        $authController->login();
+        break;
+
+    case 'signup':
+        $authController->signup();
+        break;
+
+    case 'home':
+    default:
+        // تأكد أن المسار صحيح (واش views كاينة برا App؟)
+        require_once __DIR__ . '/views/auth/index.php'; 
+        break;
+}
+?>
