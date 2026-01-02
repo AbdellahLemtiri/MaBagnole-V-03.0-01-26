aba3e46c32e2c54831dbf9899765f6480233f33b
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-02 11:54:16 +0100

    feat: Ajoute class User avec tous les methode requises

---
+++ app/Models/User.php
@@ -0,0 +1,177 @@
+<?php
+namespace App\Models;
+use App\Database\Database;
+use App\Utils\Logger;
+use PDO; 
+use Exception;
+
+class User
+{
+    protected int $idUser;
+    protected string $nomUser;
+    protected string $lastName;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+    protected string $status;
+
+    public function __construct() {}
+
+ 
+
+
+    public function getIdUser(): int
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(int $idUser): bool
+    {
+       
+        if ($idUser > 0) {
+            $this->idUser = $idUser;
+            return true;
+        }
+        return false;
+    }
+
+   
+    public function getNomUser(): string
+    {
+        return $this->nomUser;
+    }
+
+    public function setNomUser(string $nom): bool
+    {
+        
+        if (preg_match("/^[a-zA-Z\s]+$/", $nom)) {
+            $this->nomUser = strtoupper(trim($nom));
+            return true;
+        }
+        return false;
+    }
+
+   
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $prenom): bool
+    {
+       
+        if (preg_match("/^[a-zA-Z\s]+$/", $prenom)) {
+            $this->lastName = ucfirst(strtolower(trim($prenom)));
+            return true;
+        }
+        return false;
+    }
+
+    
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): bool
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = trim($email);
+            return true;
+        }
+        return false;
+    }
+
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): bool
+    {
+        if (strlen($password) >= 6) {
+            $this->password = password_hash($password, PASSWORD_DEFAULT);
+            return true;
+        }
+        return false;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): bool
+    {
+        $allowedRoles = ['admin', 'client'];
+        if (in_array(strtolower($role), $allowedRoles)) {
+            $this->role = strtolower($role);
+            return true;
+        }
+        return false;
+    }
+
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): bool
+    {
+    
+        if ($status === "1" || $status === "0") {
+            $this->status = $status;
+            return true;
+        }
+        return false;
+    }
+
+    
+    public static function Login(string $email, string $passwordRaw)
+    {
+                $conn = (new Database)->getConnection();
+
+       
+        $sql = "SELECT * FROM user WHERE email = :email";
+
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            
+            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        
+            if (!$userData) {
+                return "email_introuvable";
+            }
+
+           
+            if ($userData['status'] == 0) {
+                return "compte_bloque";
+            }
+
+          
+            if (password_verify($passwordRaw, $userData['password'])) {
+                if (session_status() === PHP_SESSION_NONE) {
+                    session_start();
+                }
+                
+                $_SESSION['userName'] = $userData['nomUser'];
+                $_SESSION['userId'] = $userData['idUser'];
+                $_SESSION['userRole'] = $userData['role'];
+
+                return $userData['role'];
+            } else {
+                return "mot_de_passe_incorrect";
+            }
+
+        } 
+        catch (Exception $e) {
+            Logger::log($e->getMessage());
+            return "erreur_technique";
+        }
+    }
+}
