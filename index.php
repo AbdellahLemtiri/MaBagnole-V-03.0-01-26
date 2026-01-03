be56e23523f4be9336a8ce646a5de9b5155c9a46
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-03 22:11:15 +0100

    feat : add a define('BASE_PATH', __DIR__); to  index.php root tht the paths are not confused

--- index.php
+++ index.php
@@ -1,33 +1,54 @@
 <?php
-
+define('BASE_PATH', __DIR__);
 require_once __DIR__ . '/App/Autoloader.php';
 App\Autoloader::register();
 
 
 use App\Controllers\AuthController;
-
+use App\Controllers\AdminController;
 
 $authController = new AuthController();
-
+$AdminController = new AdminController();
 $action = $_POST['action'] ?? $_GET['action'] ?? 'auth';
 
 switch ($action) {
+    case 'index':
+        break;
     case 'login':
         $authController->login();
         break;
-
     case 'signup':
         $authController->signup();
         break;
     case 'addVoiture':
-        $authController->signup();
+        $AdminController->setVoiture();
         break;
     case 'home':
         break;
-    case 'indexadmin':
-        require_once __DIR__ . '/views/admin/index.php';
+    case 'carAdmin':
+        $AdminController->afficherDashboard();
+        break;
+    case 'update_voiture':
+        $AdminController->updateVoiture();
+        break;
+    case 'delete_voiture':
+        $AdminController->deleteVoiture();
+        break;
+    case 'categories':
+        $AdminController->openCategory();
+        break;
+    case 'delete_categorie':
+        $AdminController->supprimerCategorie();
+        break;
+    case 'add_categorie':
+        $AdminController->ajouterCategorie();
+        break;
+    case 'update_categorie':
+        $AdminController->modifierCategorie();
+        break;
+    case 'usersAdmin';
         break;
     default:
-        require_once __DIR__ . '/views/auth/index.php';
+        require_once __DIR__ . '/views/auth/auth.php';
         break;
 }
