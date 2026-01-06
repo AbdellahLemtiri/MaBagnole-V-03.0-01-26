ccf17d831d828677f547a34d9c9ead413d00318c
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-06 09:30:09 +0100

    feat : add a methode activeUser/descativerUser a class AdminController

--- app/Controllers/VoitureController.php
+++ app/Controllers/VoitureController.php
@@ -1,28 +1,47 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Models\Voiture;
 
-class VoitureController {
+class VoitureController
+{
 
-    public function getAllVoituresJson() {
-       
+    public function getAllVoituresJson()
+    {
+
         $voitures = Voiture::getAllVoitures();
-        
-        
+
+
         $data = [];
         foreach ($voitures as $v) {
             $data[] = $v->toArray();
         }
-     
-        ob_clean(); 
+
+        ob_clean();
         header('Content-Type: application/json');
-       
+
         echo json_encode([
             'status' => 'success',
             'count' => count($data),
             'data' => $data
         ]);
-        exit; 
+        exit;
+    }
+
+    public function carList()
+    {
+        require_once 'views/client/carList.php';
+    }
+
+    public function carDetaile()
+    {
+
+        if (isset($_POST['id']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $v = new Voiture();
+            $voiture = $v->getVoitureById($id);
+            require_once 'views/client/carDetaile.php';
+        }
     }
 }
