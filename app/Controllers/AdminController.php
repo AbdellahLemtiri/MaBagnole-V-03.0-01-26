aa67b29e297fcab2e701e6d4d45879235e3c5baf
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-15 20:12:41 +0100

    feat(admin): implement tag management and reservation tracking and revision bug fix

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -1,61 +1,19 @@
 <?php
 
 namespace App\Controllers;
-
 use App\Models\Voiture;
 use App\Models\Categorie;
 use App\Models\Client;
+use App\Models\Reservation;
+use App\Models\Tag;
 use App\Utils\Logger;
 use Exception;
-
+use App\Models\Theme;
 class AdminController
 {
 
 
-    public function setVoiture()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === "POST" && $_POST['action'] === "addVoiture") {
-            if (isset($_POST['modeles']) && is_array($_POST['modeles']) && isset($_POST['marques']) && is_array($_POST['marques'])) {
-
-                $modeles = $_POST['modeles'];
-                $marques = $_POST['marques'];
-                $matricules = $_POST['matricules'];
-                $prix = $_POST['prix'];
-                $categories = $_POST['categories'];
-                $boites = $_POST['boites'];
-                $images = $_POST['images'];
-                $places = $_POST['places'];
-                $carburants = $_POST['carburants'];
-
-                $successCount = 0;
-
-
-                for ($i = 0; $i < count($modeles); $i++) {
-
-                    try {
-                        $v = new Voiture();
-                        $v->setModeleV($modeles[$i]);
-                        $v->setMarqueV($marques[$i]);
-                        $v->setMatriculeV($matricules[$i]);
-                        $v->setPrixJourV($prix[$i]);
-                        $v->setIdCV($categories[$i]);
-                        $v->setBoiteV($boites[$i]);
-                        $v->setPlacesV($places[$i]);
-                        $v->setCarburantV($carburants[$i]);
-                        $v->setImageUrlV($images[$i]);
-                        if ($v->ajouteVoiture()) {
-                            $successCount = $successCount + 1;
-                        }
-                    } catch (Exception $e) {
-                        logger::log($e->getMessage());
-                        continue;
-                    }
-                }
-            }
-            header("Location: index.php?action=carAdmin&msg=" . $successCount . " voitures ajoutées avec succès");
-            exit;
-        }
-    }
+   
     public function afficherDashboard()
     {
         $voitures = Voiture::getAllVoitures();
@@ -84,7 +42,7 @@ class AdminController
     }
     public function updateVoiture()
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'update_voiture') {
 
             $v = new Voiture();
             $v->setIdV($_POST['id']);
@@ -114,74 +72,28 @@ class AdminController
     }
 
 
-    public function ajouterCategorie()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $titre = $_POST['titre'];
-            $description = $_POST['description'];
-            $icone = $_POST['icone'];
 
-            $cat = new Categorie();
-            $cat->setTitre($titre);
-            $cat->setDescription($description);
-            $cat->setIcone($icone);
-
-            $cat->create();
-            header("Location: index.php?action=categories");
-        }
-    }
-
-    public function modifierCategorie()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = $_POST['id'];
-            $titre = $_POST['titre'];
-            $description = $_POST['description'];
-            $icone = $_POST['icone'];
-
-            $cat = new Categorie();
-            $cat->setIdC($id);
-            $cat->setTitre($titre);
-            $cat->setDescription($description);
-            $cat->setIcone($icone);
-
-            $cat->update();
-            header("Location: index.php?action=categories");
-        }
-    }
-
-
-    public function supprimerCategorie()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = $_POST['id'];
-            $cat = new Categorie();
-            $cat->setIdC($id);
-            $cat->delete();
-            header("Location: index.php?action=categories");
-        }
-    }
     public function usersAdmin()
     {
         $clients = Client::getAllClients();
-        include 'views/admin/users.php';
+        $countActif = Client::getCountUserActif();
+        require_once 'views/admin/users.php';
     }
     public function ActiveUser()
     {
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id']) && $_POST['action'] === 'activate_user') {
 
-            $id = $_POST['id']; 
+            $id = $_POST['id'];
             try {
-              
-                if ((new Client)->activateUser($id)) {
+
+                if ((new Client)->activateClient($id)) {
                     header("Location: index.php?action=usersAdmin&msg=activated");
                     exit;
                 }
-            }
-            catch (Exception $e) {
+            } catch (Exception $e) {
                 Logger::log($e->getMessage());
-              
+
                 header("Location: index.php?action=usersAdmin&msg=error");
                 exit;
             }
@@ -190,11 +102,11 @@ class AdminController
 
     public function desActiveUser()
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id']) && $_POST['action'] === 'block_user') {
 
-            $id = $_POST['id']; 
+            $id = $_POST['id'];
             try {
-                if ((new Client)->blockUser($id)) {
+                if ((new Client)->blockClient($id)) {
                     header("Location: index.php?action=usersAdmin&msg=blocked");
                     exit;
                 }
@@ -206,4 +118,93 @@ class AdminController
         }
     }
 
+    public function getAllTag()
+    {
+        $obj = new Tag();
+        $tags =  $obj->getAll();
+        require_once 'views/admin/tag.php';
+    }
+    public function addTag()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tags']) && $_POST['action'] === 'addTags') {
+
+            $tagsAvecVerguel = $_POST['tags'];
+            $tags = explode(',', $tagsAvecVerguel);
+            try {
+                foreach ($tags as $tag) {
+                    $obj = new Tag();
+                    $obj->setNomTag($tag);
+                    $obj->create();
+                }
+                header("Location: index.php?action=tagsAdmin&msg=true");
+                exit;
+            } 
+            catch (Exception $e) {
+                Logger::log($e->getMessage());
+                header("Location: index.php?action=tagsAdmin&msg=false");
+                exit;
+            }
+        }else{
+             header("Location: index.php?action=tagsAdmin&msg=incomplete");
+                exit;
+        }
+    }
+
+    public function deletTag()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'deleteTag') {
+            $id = $_POST['id'];
+            $tag = new Tag();
+            $tag->setidTag($id);
+            if ($tag->delete()) {
+                header("Location: index.php?action=tagsAdmin&msg=true");
+                exit;
+            } else {
+                header("Location: index.php?action=tagsAdmin&msg=false");
+                exit;
+            }
+        }
+        else{
+             header("Location: index.php?action=tagsAdmin&msg=incomplete");
+                exit;
+        }
+    }
+    public function updateTag()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom']) && $_POST['action'] === 'updateTag') {
+            $tag = new Tag();
+            $id = $_POST['id'];
+            $nom = $_POST['nom'];
+            $tag->setidTag($id);
+            $tag->setNomTag($nom);
+             if ($tag->updateTag()) {
+                header("Location: index.php?action=tagsAdmin&msg=true");
+                exit;
+            } else {
+                header("Location: index.php?action=tagsAdmin&msg=false");
+                exit;
+            }
+        }
+        else{
+             header("Location: index.php?action=tagsAdmin&msg=incomplete");
+                exit;
+        }
+    }
+ public function affichierToutThemeAdmin()
+    {
+        $obj = new Theme();
+        $themes = $obj->getAllTheme();
+        require_once 'views/admin/Theme.php';
+    }
+
+    public function getAllReservation() {
+        $resrvation = new Reservation();
+     $reservations =   $resrvation->getAllReservationsBY();
+        require_once 'views/admin/dashboard.php';
+    }
+
+       public function reservationAdmin()
+    {
+        require_once 'views/admin/dashboard.php';
+    }
 }
