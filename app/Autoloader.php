4cccf82b163258558496591069212a61a3e12450
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-15 20:31:06 +0100

    fix(autoloader): add a register function  and  bug fix

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -1,13 +1,11 @@
 <?php
 namespace App;
-
 class Autoloader
 {
     public static function register()
     {
         spl_autoload_register([self::class, 'autoload']);
     }
-
     private static function autoload($class)
     {
         $prefix = 'App\\';
@@ -17,6 +15,7 @@ class Autoloader
         if (strncmp($prefix, $class, $len) !== 0) {
             return; 
         }    
+        
         $relative_class = substr($class, $len); 
         $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
         if (file_exists($file)) {
