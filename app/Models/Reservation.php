63768472429c531c18c414aa55908a6ea11050e2
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-15 19:47:31 +0100

    feat(reservation): add status update functionality and comprehensive reservation listing

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -7,18 +7,23 @@ use PDOException;
 use Exception;
 use PDO;
 use App\Utils\Logger;
+use App\Models\Voiture;
+use App\Models\Client;
+
 class Reservation
+
 {
 
-    private $idReservation;
-    private $dateDebut;
-    private $dateFin;
-    private $lieuChange;
-    private $status;
-    private $idVoiture;
-    private $idUser;
-    private $totalPrix;
-   
+    private int $idReservation;
+    private string $dateDebut;
+    private string $dateFin;
+    private string $lieuChange;
+    private string $status;
+    private int  $idVoiture;
+    private int $idUser;
+    private float $totalPrix;
+    private Client $Client;
+    private Voiture $voiture;
     public function getTotalPrix()
     {
         return $this->totalPrix;
@@ -36,6 +41,28 @@ class Reservation
     }
 
 
+    public function getClient(): Client
+    {
+        return $this->Client;
+    }
+
+    public function getVoiture(): Voiture
+    {
+        return $this->voiture;
+    }
+
+
+    public function setClient(Client $Client)
+    {
+        $this->Client = $Client;
+    }
+
+    public function setVoiture(Voiture $voiture)
+    {
+        $this->voiture = $voiture;
+    }
+
+
     public function getIdReservation()
     {
         return $this->idReservation;
@@ -45,7 +72,7 @@ class Reservation
     {
         return $this->dateDebut;
     }
-    public function setDateDebut($date)
+    public function setDateDebut(string $date)
     {
         $this->dateDebut = $date;
     }
@@ -54,7 +81,7 @@ class Reservation
     {
         return $this->dateFin;
     }
-    public function setDateFin($date)
+    public function setDateFin(string $date)
     {
         $this->dateFin = $date;
     }
@@ -63,7 +90,7 @@ class Reservation
     {
         return $this->lieuChange;
     }
-    public function setLieuChange($lieu)
+    public function setLieuChange(string $lieu)
     {
         $this->lieuChange = $lieu;
     }
@@ -72,7 +99,7 @@ class Reservation
     {
         return $this->status;
     }
-    public function setStatus($status)
+    public function setStatus(string $status)
     {
         $this->status = $status;
     }
@@ -81,46 +108,41 @@ class Reservation
     {
         return $this->idVoiture;
     }
-    public function setIdVoiture($id)
+    public function setIdVoiture(int $id)
     {
         $this->idVoiture = $id;
     }
+
+
+
 
     public function getIdUser()
     {
         return $this->idUser;
     }
-    public function setIdUser($id)
+    public function setIdUser(int $id)
     {
         $this->idUser = $id;
     }
 
+    public function addReseravtion()
+    {
+        $sql = "CALL AjouterReservation(:dateDebut, :dateFin, :lieu, :total, :idVoiture, :idUser)";
+        try {
+            $stmt = $this->db->prepare($sql);
 
-   public function ajouterReservation() {
-
-    if ($this->dateFin <= $this->dateDebut) {
-      Logger::log("reservation : La date de fin doit être supérieure à la date de début.");
+            return $stmt->execute([
+                ':dateDebut' => $this->dateDebut,
+                ':dateFin'   => $this->dateFin,
+                ':lieu'      => $this->lieuChange,
+                ':total'     => $this->totalPrix,
+                ':idVoiture' => $this->idVoiture,
+                ':idUser'    => $this->idUser
+            ]);
+        } catch (Exception $e) {
+            Logger::log($e->getMessage());
+        }
     }
-
-    
-    $sql = "CALL AjouterReservation(:dateDebut, :dateFin, :lieu, :total, :idVoiture, :idUser)";
-   try{
-   $stmt = $this->db->prepare($sql);
-    
-    return $stmt->execute([
-        ':dateDebut' => $this->dateDebut,
-        ':dateFin'   => $this->dateFin,
-        ':lieu'      => $this->lieuChange,
-        ':total'     => $this->totalPrix,
-        ':idVoiture' => $this->idVoiture,
-        ':idUser'    => $this->idUser
-    ]);
-
-   }catch(Exception $e){
-      Logger::log($e->getMessage());
-   } 
- 
-}
 
 
     public function getReservationsByUser($userId)
@@ -156,15 +178,12 @@ class Reservation
     {
         $sql = "SELECT COUNT(*) as count FROM reservations 
                 WHERE idVoiture = :idV 
-                AND status != 'annulee'
-                AND (
-                    (dateDebut BETWEEN :start AND :end) OR 
-                    (dateFin BETWEEN :start AND :end) OR
-                    (:start BETWEEN dateDebut AND dateFin)
+                AND status != 'annulee' AND ((dateDebut BETWEEN :start AND :end) OR (dateFin BETWEEN :start AND :end) OR (:start BETWEEN dateDebut AND dateFin)
                 )";
 
         $stmt = $this->db->prepare($sql);
         $stmt->execute([
+
             ':idV' => $voitureId,
             ':start' => $start,
             ':end' => $end
@@ -172,5 +191,54 @@ class Reservation
 
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         return $result['count'] == 0;
+    }
+
+    public function getAllReservationsBY()
+    {
+        $db = Database::getInstance()->getConnection();
+        $sql = "SELECT r.*, v.marque, v.modele,v.image,u.email, u.name, u.LastName 
+            FROM reservations r
+            JOIN voitures v ON r.idVoiture = v.idV
+            JOIN users u ON r.idUser = u.idUser
+            ORDER BY r.idReservation DESC";
+        try {
+            $stmt = $db->query($sql);
+
+            $resultat = $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
+
+            foreach ($resultat as $res) {
+                $Client = new Client();
+                $voiture = new Voiture();
+                $Client->setName($res->name);
+                $Client->setLastName($res->LastName);
+                $Client->setEmail($res->email);
+                $voiture->setMarqueV($res->marque);
+                $voiture->setModeleV($res->modele);
+                $voiture->setImageUrlV($res->image);
+                $res->setClient($Client);
+                $res->setVoiture($voiture);
+            }
+            return $resultat;
+        } 
+        catch (Exception $e) {
+            Logger::log($e->getMessage());
+            return [];
+        }
+    }
+    public function updateStatus($id,$status) : bool
+    {
+        $db = Database::getInstance()->getConnection();
+
+        $sql = "UPDATE reservations set status = :status WHERE idReservation = :id";
+        try {
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':status',$status );
+        $stmt->bindValue(':id',$id );
+        $stmt->execute();
+        return true;
+        }catch(Exception $e){
+            Logger::log($e->getMessage());
+            return false;
+        }
     }
 }
