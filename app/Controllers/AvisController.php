7a76316bd5f7a5510f40ada71be0907af61292da
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-16 00:12:38 +0100

    feat(avis): update paths for review submissions

--- app/Controllers/AvisController.php
+++ app/Controllers/AvisController.php
@@ -12,10 +12,10 @@ class AvisController
         if (isset($_POST['commentaire'], $_POST['note'], $_POST['idReservation'], $_POST['idUser']) && !empty($_POST['commentaire'])) {
 
             $commentaire = $_POST['commentaire'];
-            $note = $_POST['note'];
-            $idReservation = $_POST['idReservation'];
-            $idUser = $_POST['idUser'];
-            $idVoiture = $_POST['idVoiture'];
+            $note =(int) $_POST['note'];
+            $idReservation =(int) $_POST['idReservation'];
+            $idUser = (int)$_POST['idUser'];
+            $idVoiture = (int)$_POST['idVoiture'];
             $avis = new Avis();
             $avis->setCommentaire($commentaire);
             $avis->setNote($note);
@@ -23,39 +23,54 @@ class AvisController
             $avis->setIdClient($idUser);
             $avis->setIdVoiture($idVoiture);
             if ($avis->addAvis()) {
-                header('Location: index.php?action=reservationUser&msj=vrai');
+                header('Location: /MaBagnoleV1/reservationUser&msj=true');
                 exit();
             } else {
-                header('Location: index.php?action=reservationUser&msj=faux');
+                header('Location: /MaBagnoleV1/reservationUser&msj=false');
                 exit();
             }
         }
     }
-
-    //idAvis note commentaire updateAvis
+ 
 
     public function updateAvis()
     {
         if ($_POST['action'] === 'updateAvis' && isset($_POST['idAvis'])) {
 
             $avis = new Avis();
-            $idVoiture = $_POST['idVoiture'];
-            $idAvis = $_POST['idAvis'];
-            $note = $_POST['note'];
+            $idVoiture =(int) $_POST['idVoiture'];
+            $idAvis = (int)$_POST['idAvis'];
+            $note = (int)$_POST['note'];
             $commentaire = $_POST['commentaire'];
             $avis->setIdAvis($idAvis);
             $avis->setNote($note);
             $avis->setCommentaire($commentaire);
             if ($avis->updateAvis()) {
-                header('Location: index.php?action=CarDetaile&msj=updateAvisvrai&id='.$idVoiture);
+                header('Location: /MaBagnoleV1/CarDetaile?msg=true&id='.$idVoiture);
                 exit();
             } else {
-                header('Location: index.php?action=CarDetaile&msj=UpdateAvisfaux&id='.$idVoiture);
+                header('Location: /MaBagnoleV1/CarDetaile?msg=false&id='.$idVoiture);
                 exit();
             }
-        } else {
-            header('Location: index.php?action=CarDetaile&msj=Incomplete&id=');
-            exit();
-        }
+        } 
+    }
+
+
+      public function deleteAvis()
+    {
+        if ($_POST['action'] === 'deleteAvis' && isset($_POST['idAvis'])) {
+
+            $avis = new Avis();
+            $idVoiture = (int)$_POST['idVoiture'];
+            $idAvis =(int) $_POST['idAvis'];
+            $avis->setIdAvis($idAvis);
+            if ($avis->softdelet()) {
+                header('Location: /MaBagnoleV1/CarDetaile?msg=true&id='.$idVoiture);
+                exit();
+            } else {
+                header('Location: /MaBagnoleV1/CarDetaile?msg=false&id='.$idVoiture);
+                exit();
+            }
+        } 
     }
 }
