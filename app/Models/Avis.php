b7c349e8bdca7dbe3339f1d19ca6098d8d56e0a0
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-15 17:29:28 +0100

    feat(avis): add review update functionality and user ownership validation logic

--- app/Models/Avis.php
+++ app/Models/Avis.php
@@ -6,7 +6,10 @@ use App\Config\Database;
 use App\Utils\Logger;
 use DateTime;
 use Exception;
-use FFI\Exception as FFIException;
+use App\Models\User;
+use App\Models\Voiture;
+
+use PDO;
 
 class Avis
 {
@@ -14,17 +17,30 @@ class Avis
     private int $idAvis;
     private string $commentaire;
     private int $note;
-    private DateTime $datePublication;
+    private string $datePublication;
     private int $status;
     private int $idReservation;
     private  int $idClient;
-
-
+    private $db;
+    private User $user;
+    private int $idVoiture;
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+    public function getUser()
+    {
+        return $this->user;
+    }
     public function getIdAvis()
     {
         return $this->idAvis;
     }
-    public function setIdAvis($idAvis)
+    public function setIdAvis(int $idAvis)
     {
         $this->idAvis = $idAvis;
     }
@@ -33,7 +49,7 @@ class Avis
     {
         return $this->commentaire;
     }
-    public function setCommentaire($commentaire)
+    public function setCommentaire(string $commentaire)
     {
         $this->commentaire = $commentaire;
     }
@@ -42,7 +58,7 @@ class Avis
     {
         return $this->note;
     }
-    public function setNote($note)
+    public function setNote(int $note)
     {
         $this->note = $note;
     }
@@ -51,7 +67,7 @@ class Avis
     {
         return $this->datePublication;
     }
-    public function setDatePublication($datePublication)
+    public function setDatePublication(string $datePublication)
     {
         $this->datePublication = $datePublication;
     }
@@ -61,7 +77,7 @@ class Avis
     {
         return $this->status;
     }
-    public function setStatus($status)
+    public function setStatus(int $status)
     {
         $this->status = $status;
     }
@@ -71,7 +87,7 @@ class Avis
     {
         return $this->idReservation;
     }
-    public function setIdReservation($idReservation)
+    public function setIdReservation(int $idReservation)
     {
         $this->idReservation = $idReservation;
     }
@@ -85,14 +101,22 @@ class Avis
     {
         $this->idClient = $idClient;
     }
+    
 
-
+   public function getIdVoiture()  
+    {
+        return $this->idVoiture;
+    }
+    public function setIdVoiture($idVoiture)
+    {
+        $this->idVoiture = $idVoiture;
+    }
 
     public function addAvis(): bool
     {
-        $conn = Database::getInstance()->getConnection();
-        $sql = "INSERT INTO avis (commentaire, note, status, idReservation, idClient) 
-            VALUES (:commentaire, :note,1, :idRes, :idClient)";
+        $conn = $this->db->getConnection();
+        $sql = "INSERT INTO avis (commentaire, note, status, idReservation, idClient,idVoiture) 
+            VALUES (:commentaire, :note,1, :idRes, :idClient,:idVoiture)";
 
 
         try {
@@ -102,7 +126,8 @@ class Avis
                 ':commentaire' => $this->commentaire,
                 ':note'        => $this->note,
                 ':idRes'       => $this->idReservation,
-                ':idClient'    => $this->idClient
+                ':idClient'    => $this->idClient,
+                ':idVoiture'    => $this->idVoiture
             ]);
 
             return $result;
@@ -111,9 +136,9 @@ class Avis
             return false;
         }
     }
-    public function softdelet():bool
+    public function softdelet(): bool
     {
-        $conn = Database::getInstance()->getConnection();
+        $conn = $this->db->getConnection();
         $sql = "UPDATE avis set sataus = 0 Where idAvis = :id";
 
         try {
@@ -127,5 +152,90 @@ class Avis
             return false;
         }
     }
+
+    public function getAllAVisByIdVoiture($id): array
+    {
+        $conn = $this->db->getConnection();
+
+        $sql = "SELECT a.*, u.name, u.LastName,u.idUser 
+            FROM avis a 
+            INNER JOIN users u ON a.idClient = u.idUser 
+            INNER JOIN voitures v ON a.idVoiture = v.idV 
+            WHERE v.idV = :id AND a.status = 1";
+
+        try {
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $lesAvis = [];
+            foreach ($result as $res) {
+                $avis = new Avis();
+                $user = new User();
+                $avis->setCommentaire($res['commentaire']);
+                $avis->setDatePublication($res['datePublication']);
+                $avis->setNote($res['note']);
+                $avis->setIdAvis($res['idAvis']);
+                $avis->setIdClient($res['idClient']);
+                $avis->setIdVoiture($res['idVoiture']);
+                $user->setLastName($res['LastName']);
+                $user->setName($res['name']);
+                $avis->setUser($user);
+
+                $lesAvis[] = $avis;
+            }
+
+            return $lesAvis;
+        } catch (Exception $e) {
+            Logger::log($e->getMessage());
+            return [];
+        }
+    }
+
+
     
+    public function updateAvis():bool
+    {
+        $conn = $this->db->getConnection();
+        $sql = "UPDATE avis SET commentaire = :commentaire,note = :note WHERE idAvis = :id";
+
+        try{
+             $stmt = $conn->prepare($sql);
+             $stmt->execute([
+                ':id' => $this->idAvis,
+                ':commentaire' => $this->commentaire,
+                ':note' => $this->note
+             ]);
+             return true;
+        }
+        catch(Exception $e){
+            Logger::log($e->getMessage());
+            return false;
+        }
+    }
+
+    public  function checkAvisUser(int $idClient, int $idAvis): bool
+    {    
+        $conn = $this->db->getConnection();
+        $sql = "SELECT COUNT(*) FROM avis WHERE idAvis = :idAvis AND idClient = :idClient";
+        
+        try 
+        {
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':idAvis', $idAvis, PDO::PARAM_INT);
+            $stmt->bindValue(':idClient', $idClient, PDO::PARAM_INT);
+            $stmt->execute();
+            $count = $stmt->fetchColumn();
+            return $count > 0;
+        }
+        catch (Exception $e) 
+        {
+            Logger::log($e->getMessage());
+            return false;
+        }
+    }   
+    
+
 }
