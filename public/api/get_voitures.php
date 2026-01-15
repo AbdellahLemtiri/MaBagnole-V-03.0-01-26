5ff225304e1414683dd1ffc40d57713bc0b19eaa
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-01-15 20:56:02 +0100

    Update get_voitures script

--- public/api/get_voitures.php
+++ public/api/get_voitures.php
@@ -3,11 +3,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
-    require_once __DIR__ . '/../../vendor/autoload.php';
-} 
 
-elseif (file_exists(__DIR__ . '/../../App/Autoloader.php')) {
+
+if (file_exists(__DIR__ . '/../../App/Autoloader.php')) {
     require_once __DIR__ . '/../../App/Autoloader.php';
     
     if (class_exists('App\Autoloader')) {
@@ -22,6 +20,7 @@ else {
 }
 
 use App\Controllers\VoitureController;
+use App\Utils\Logger;
 
 try {
 
@@ -29,6 +28,7 @@ try {
     $controller->getAllVoituresJson();
 
 } catch (Throwable $e) {
+    Logger::log($e->getMessage());
     http_response_code(500);
     echo json_encode([
         'status' => 'error',
